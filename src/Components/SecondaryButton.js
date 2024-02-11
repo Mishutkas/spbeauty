8fb73bf5ace2579dc2c25d2c1d9b3e0e9ad93125
Mishutkas <152716632+Mishutkas@users.noreply.github.com>
2024-02-11 15:32:33 +0000

    Обновила стиль SecondaryButton

--- src/Components/SecondaryButton.js
+++ src/Components/SecondaryButton.js
@@ -5,16 +5,18 @@ function SecondaryButton(props) {
     // Здесь устанавливается минимальная ширина для кнопки
     let minWidth = props.minWidth === undefined ? '144px' : props.minWidth;
     const Button = styled.div`
-        background: #F2F2F2;
-        border: 1px solid #DFDFDF;
+        border: 1px solid #64748B;
         box-shadow: inset 0px 1px 2px rgba(10, 15, 26, 0.08);
         min-width: ${minWidth};
         color: #152842;
+        border-radius: 10px;
         &:hover {
-            background: linear-gradient(180deg, #DFEAF1 0%, #CFDDE8 100%);
+            opacity: 0.8;
+            transition: opacity 0.3s ease;
         }
         &:active {
-            background: #DFDFDF;
+            opacity: 0.6;
+            transition: opacity 0.3s ease;
         }
     `;
     return (
