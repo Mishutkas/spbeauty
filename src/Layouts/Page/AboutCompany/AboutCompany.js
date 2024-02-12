fc39e1540c24dc95921e85fc850a3f1c1db7aa3a
Mishutkas <152716632+Mishutkas@users.noreply.github.com>
2024-02-12 23:28:18 +0000

    2 столба

--- src/Layouts/Page/AboutCompany/AboutCompany.js
+++ src/Layouts/Page/AboutCompany/AboutCompany.js
@@ -6,7 +6,7 @@ function AboutCompany() {
         <div>
             <Heading/> {/* Заголовок и кнопка справа */}
             
-            <div className='d-flex flex-column mt-5'>
+            <div className='class="row" mt-5'>
                 <General/>  {/* Основная информация вроде названия, типа и телефона */}
                 <Addresses/> {/* Список доступных адресов заведений */}
             </div>
