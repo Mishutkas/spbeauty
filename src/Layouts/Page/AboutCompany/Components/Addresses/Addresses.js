6120f083cf9da77d5d7f90e17bc1037ec0db0f6e
Mishutkas <152716632+Mishutkas@users.noreply.github.com>
2024-02-15 13:37:15 +0000

    блок адресесс

--- src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
+++ src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
@@ -3,12 +3,12 @@ import SecondaryButton from "../../../../../Components/SecondaryButton";
 import ListElement from "./ListElement";
 function Addresses() {
     return (
-        <div className='d-inline-flex flex-column gap-2 mt-4 p-2 adressesborder'>
+        <div className='d-inline-flex flex-column gap-5 mt-4 p-4 adressesborder'>
             <div className='title'>Адреса заведений</div>
             <div className='d-inline-flex flex-column gap-3'>
                 <ListElement>Санкт-Петербург, Невский пр-т. 28</ListElement>
                 <ListElement>Санкт-Петербург, Казанская ул, 7</ListElement>
-                <PrimaryButton>Добавить филиал</PrimaryButton>
+                <SecondaryButton>Добавить филиал</SecondaryButton>
             </div>
         </div>
     );
